4957c5c076c293874fafd50508f6400a6bed0085
Alfred Asare <alfredasare101@gmail.com>
2020-10-13 07:02:55 +0000

    moved page tests to page-tests folder

--- page-tests/index.test.tsx
+++ page-tests/index.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from '@testing-library/react';
 
-import Home from "./index";
+import Home from "../pages";
 
 describe('home page tests', () => {
     test('snapshot test of home page', () => {
